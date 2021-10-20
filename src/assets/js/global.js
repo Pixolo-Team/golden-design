80324484a6ece1d4e960d11cca6037c1221942a1
Princeton Gomes <adarsh.pixolo@gmail.com>
2021-10-20 19:56:59 +0530

    development

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,23 +1,43 @@
 var data;
-fetch("https://goldendesigninteriors.com/data/website-data.json")
-    .then((response) => {
-        if (response.status !== 200) {
-            console.log("Request failed.", response.status);
-            return;
+
+function updateTestimonials() {
+    let i = 0;
+    for (let testimonial of data.testimonials) {
+        if (i % 2 == 0) {
+            $('.section-6-scroll').children('.section6-paragraph').text(testimonial.review);
+            $('.section-6-scroll').children('.line1').text(testimonial.client_name);
+            $('.section-6-scroll').children('.line2').text(testimonial.client_work);
+        } else {
+            $('.section6-scroll-2').children('.section6-paragraph').text(testimonial.review);
+            $('.section6-scroll-2').children('.line1').text(testimonial.client_name);
+            $('.section6-scroll-2').children('.line2').text(testimonial.client_work);
         }
-        response.json().then((responseData) => {
-            data = responseData;
-            for (count of data.counts) {
-                console.log(document.getElementsByClassName("number-box").length);
-                document.getElementsByClassName("number-box")[0].innerHTML += `
-                    <div class="left-line-box">
-                        <div class="numbers">${count.value}</div>
-                        <div class="numbers-discription">${count.title}</div>
-                    </div>
-                `;
+        i += 1;
+    }
+}
+function getWebsiteData(url) {
+    fetch(url)
+        .then(function(response) {
+            if (response.status !== 200) {
+                console.log("Request failed.", response.status);
+                return;
             }
+            response.json().then(function(responseData) {
+                data = responseData;
+                for (count of data.counts) {
+                    document.getElementsByClassName("number-box")[0].innerHTML += `
+                        <div class="left-line-box">
+                            <div class="numbers">${count.value}</div>
+                            <div class="numbers-discription">${count.title}</div>
+                        </div>
+                    `;
+                }
+                updateTestimonials();
+            })
         })
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+getWebsiteData("https://goldendesigninteriors.com/data/website-data.json");
