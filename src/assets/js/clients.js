0a083e9a7bdc2a9b0da8c0783c34b5fc1aa99872
Aliya9702 <aliya.pixolo@gmail.com>
2022-03-14 21:31:05 +0530

    fixes

--- src/assets/js/clients.js
+++ src/assets/js/clients.js
@@ -1,19 +1,36 @@
-$(document).ready(()=>{
-    $('.clients-logo-slider').slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        responsive: [
-          
-          {
-            breakpoint: 697,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-        ]
-      });
+$(document).ready(() => {
+  $('.clients-logo-slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
 })
---
+++ src/assets/js/individual-projects.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $('.image-wrapper').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+})
